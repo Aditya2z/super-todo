a868bc45167a87721d165dbe162ac1d04ebcf1b1
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2025-08-31 12:13:19 +0530

    TD-2--Chore: Add API with Express

---
+++ apps/api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+//Inbuilt middlewares
+app.use(express.json()); // parse json and put into req.body
+app.use(express.urlencoded({ extended: false })); // handle form data
+app.use(express.static(path.join(__dirname, "public"))); // handle static assets
+
+//Third party middlewares
+//cokieparser and express-session
+
+// health check route
+app.get("/", (req, res) => {
+  res.json({ message: "Super-Todo API is running 🚀" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ API server running at http://localhost:${PORT}`);
+});
